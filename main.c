c32b64852dbf7b19e5381ab2db469b45fdf5fe88
ENESp <enes.palabiyik@bbz-sh.ch>
2022-11-12 10:17:23 +0100

    neeuu

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int Max(int Zahl1, int Zahl2)
+
+
+
+int main() {
+    int Zahl1 = 0 ;
+    int Zahl2 = 0 ;
+    int Ergebnis;
+
+
+    printf("Geben sie 2 Zahlen ein:\n");
+    scanf("%d , %d", &Zahl1, &Zahl2);
+    printf("%d , %d\n\n", Zahl1 , Zahl2);
+
+
+
+    return 0;
+}
